ca1446529214c60edb034e4a7be6ed44aa827b49
EddieCornelious <eulerissmart@gmail.com>
2019-06-11 14:49:07 -0400

    added loader and animations

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,23 @@ import skillsData from "./skills_data.js";
 import Projects from "./Projects.js";
 
 class App extends React.Component {
+  state = {
+    loadedHero: false
+  };
+  renderLoadedApp() {
+    this.setState({
+      loadedHero: true
+    });
+  }
+
   render() {
+    if (!this.state.loadedHero) {
+      setTimeout(() => {
+        this.renderLoadedApp();
+      }, 4000);
+      return null;
+    }
+
     return (
       <React.Fragment>
         <Header />
