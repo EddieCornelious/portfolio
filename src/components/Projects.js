ffb1995d6dc5318fd7ba43e5e96d00532e93dbf7
EddieCornelious <eulerissmart@gmail.com>
2019-06-11 12:50:30 -0400

    added project assets

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,17 @@
 import "../styles/Projects.scss";
 import React from "react";
 
+const ProjectCard = () => {
+  return (
+    <div className="col-sm-4">
+      <div className="project__card">
+        <div className="project__card__top">&nbsp;</div>
+
+        <div className="project__card__bottom">&nbsp;</div>
+      </div>
+    </div>
+  );
+};
 const Projects = () => {
   return (
     <section className="projects__section">
@@ -8,6 +19,13 @@ const Projects = () => {
         <h2>
           <span className="section__header__top__text">PROJECT PORTFOLIO</span>
         </h2>
+      </div>
+      <div className="container-fluid">
+        <div className="row">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
       </div>
     </section>
   );
