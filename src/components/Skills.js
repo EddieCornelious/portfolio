41e6bd3f33e4f30abeb68921f2b75b81b000cf15
EddieCornelious <eulerissmart@gmail.com>
2019-06-13 08:53:57 -0400

    minor css fixes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,18 +5,20 @@ import Bounce from "react-reveal/Bounce";
 const SkillsCard = ({ first, data }) => {
   return (
     <div className="col-sm-3">
-      <div className={first ? "skills__card card__first" : "skills__card"}>
-        <div className="skills__card__overlay">&nbsp;</div>
-        <div className="skills__card__content">
-          <i className={data.font} />
-          <h3>{data.title}</h3>
-          <ul className="skills__list">
-            {data.skills.map(e => {
-              return <li key={e}>{e}</li>;
-            })}
-          </ul>
+      <Bounce bottom>
+        <div className={first ? "skills__card card__first" : "skills__card"}>
+          <div className="skills__card__overlay">&nbsp;</div>
+          <div className="skills__card__content">
+            <i className={data.font} />
+            <h3>{data.title}</h3>
+            <ul className="skills__list">
+              {data.skills.map(e => {
+                return <li key={e}>{e}</li>;
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
+      </Bounce>
     </div>
   );
 };
@@ -25,18 +27,13 @@ const Skills = ({ data }) => {
     <section className="skills__section">
       <div className="container skills__unskew">
         <div className="row">
-          <Bounce bottom>
-            <SkillsCard data={data[1]} first={true} />
-          </Bounce>
-          <Bounce bottom>
-            <SkillsCard data={data[2]} />
-          </Bounce>
-          <Bounce bottom>
-            <SkillsCard data={data[3]} />
-          </Bounce>
-          <Bounce bottom>
-            <SkillsCard data={data[4]} />
-          </Bounce>
+          <SkillsCard data={data[1]} first={true} />
+
+          <SkillsCard data={data[2]} />
+
+          <SkillsCard data={data[3]} />
+
+          <SkillsCard data={data[4]} />
         </div>
       </div>
     </section>
