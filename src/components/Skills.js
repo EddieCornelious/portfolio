bd7e46c5068eaec32c1a77f266b0f6c0e41dff6e
EddieCornelious <eulerissmart@gmail.com>
2019-06-13 14:39:04 -0400

    changed skills animations

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,24 +1,22 @@
 import "../styles/Skills.scss";
 import React from "react";
-import Bounce from "react-reveal/Bounce";
+import Slide from "react-reveal/Slide";
 
 const SkillsCard = ({ first, data }) => {
   return (
     <div className="col-sm-3">
-      <Bounce bottom>
-        <div className={first ? "skills__card card__first" : "skills__card"}>
-          <div className="skills__card__overlay">&nbsp;</div>
-          <div className="skills__card__content">
-            <i className={data.font} />
-            <h3>{data.title}</h3>
-            <ul className="skills__list">
-              {data.skills.map(e => {
-                return <li key={e}>{e}</li>;
-              })}
-            </ul>
-          </div>
+      <div className={first ? "skills__card card__first" : "skills__card"}>
+        <div className="skills__card__overlay">&nbsp;</div>
+        <div className="skills__card__content">
+          <i className={data.font} />
+          <h3>{data.title}</h3>
+          <ul className="skills__list">
+            {data.skills.map(e => {
+              return <li key={e}>{e}</li>;
+            })}
+          </ul>
         </div>
-      </Bounce>
+      </div>
     </div>
   );
 };
@@ -27,13 +25,17 @@ const Skills = ({ data }) => {
     <section className="skills__section">
       <div className="container skills__unskew">
         <div className="row">
-          <SkillsCard data={data[1]} first={true} />
+          <Slide left>
+            <SkillsCard data={data[1]} first={true} />
+          </Slide>
 
           <SkillsCard data={data[2]} />
 
           <SkillsCard data={data[3]} />
 
-          <SkillsCard data={data[4]} />
+          <Slide right>
+            <SkillsCard data={data[4]} />
+          </Slide>
         </div>
       </div>
     </section>
