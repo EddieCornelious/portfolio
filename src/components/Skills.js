794b665e4b6b3be95d9bfcdfc3799d6fc0fc77b7
EddieCornelious <eulerissmart@gmail.com>
2019-06-19 10:55:30 -0400

    css fixes and added and resized large images

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,11 +2,11 @@ import "../styles/Skills.scss";
 import React from "react";
 import Slide from "react-reveal/Slide";
 
-const SkillsCard = ({ first, data }) => {
+const SkillsCard = ({ first, data, id }) => {
   return (
-    <div className="col-sm-3">
+    <div className="col-md-3">
       <div className={first ? "skills__card card__first" : "skills__card"}>
-        <div className="skills__card__overlay">&nbsp;</div>
+        <div className={"skills__card__overlay overlay__" + id}>&nbsp;</div>
         <div className="skills__card__content">
           <i className={data.font} />
           <h3>{data.title}</h3>
@@ -23,19 +23,21 @@ const SkillsCard = ({ first, data }) => {
 const Skills = ({ data }) => {
   return (
     <section className="skills__section">
-      <div className="container skills__unskew">
+      <div className="container">
         <div className="row">
-          <Slide left>
-            <SkillsCard data={data[1]} first={true} />
-          </Slide>
+          <div className="skills__unskew">
+            <Slide left>
+              <SkillsCard id="1" data={data[1]} first={true} />
+            </Slide>
 
-          <SkillsCard data={data[2]} />
+            <SkillsCard id="2" data={data[2]} />
 
-          <SkillsCard data={data[3]} />
+            <SkillsCard id="3" data={data[3]} />
 
-          <Slide right>
-            <SkillsCard data={data[4]} />
-          </Slide>
+            <Slide right>
+              <SkillsCard id="4" data={data[4]} />
+            </Slide>
+          </div>
         </div>
       </div>
     </section>
--- src/components/project_data.js
+++ src/components/project_data.js
@@ -7,21 +7,21 @@ import demo2 from "../images/manuel.mp4";
 import demo3 from "../images/real_estate.mp4";
 import demo4 from "../images/stream.mp4";
 const data = {
-  1: {
+  3: {
     title: "Manuel's",
     info: "A restaurant site I built for a client using HTML, CSS, React",
     img_src: manuels,
     github: "https://github.com/EddieCornelious/restaurant",
     demo: demo2
   },
-  2: {
+  1: {
     title: "John and Wick",
     info: "A Real-Estate site built using HTML, NodeJS, CSS, and React",
     img_src: wick,
     github: "https://github.com/EddieCornelious/homes",
     demo: demo3
   },
-  3: {
+  2: {
     title: "DashBoard Box",
     info:
       "A dashboard/portal for a website so user can view popular Twitch games and streams live",
