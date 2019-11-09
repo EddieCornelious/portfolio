56ac19d95e25da848dd10b2dd876b4b1d783698d
EddieCornelious <eulerissmart@gmail.com>
2019-11-09 09:37:46 -0500

    Updates

---
+++ precache-manifest.7078459336e44cfde0647219f157d622.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "f56062c8cdced5a162cf",
+    "url": "/portfolio/static/css/main.ab57d3f1.chunk.css"
+  },
+  {
+    "revision": "f56062c8cdced5a162cf",
+    "url": "/portfolio/static/js/main.5d21eaf3.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "b701ccf20d6ca5dd741a",
+    "url": "/portfolio/static/js/2.fb0bff41.chunk.js"
+  },
+  {
+    "revision": "72808ec0f39241421557b0da7aba62a2",
+    "url": "/portfolio/static/media/nodejs.72808ec0.png"
+  },
+  {
+    "revision": "db57b69ee2bc54a6910a601628908762",
+    "url": "/portfolio/static/media/javascript.db57b69e.png"
+  },
+  {
+    "revision": "1e71b45938cc2ffd396886c5ffae279f",
+    "url": "/portfolio/static/media/react.1e71b459.png"
+  },
+  {
+    "revision": "b3d5f725e1af52d63f87abf460303e2e",
+    "url": "/portfolio/static/media/manuels.b3d5f725.PNG"
+  },
+  {
+    "revision": "2b3b4453961e7858f362ffdd80a3bb40",
+    "url": "/portfolio/static/media/realtor.2b3b4453.PNG"
+  },
+  {
+    "revision": "adfaf2dd5f485b9d253303fb10faf569",
+    "url": "/portfolio/static/media/stream.adfaf2dd.PNG"
+  },
+  {
+    "revision": "00665fa1cf125da9acb7f5a156bf43d1",
+    "url": "/portfolio/static/media/gym.00665fa1.PNG"
+  },
+  {
+    "revision": "92880e30c1251befdc7b4f66415565c9",
+    "url": "/portfolio/static/media/gym.92880e30.mp4"
+  },
+  {
+    "revision": "09f022a12ca4e397681c051b080956b3",
+    "url": "/portfolio/static/media/manuel.09f022a1.mp4"
+  },
+  {
+    "revision": "a5883edee241f0c3298467c58fe69c65",
+    "url": "/portfolio/static/media/real_estate.a5883ede.mp4"
+  },
+  {
+    "revision": "aaceeb7832be478f7277035ae588e75f",
+    "url": "/portfolio/static/media/stream.aaceeb78.mp4"
+  },
+  {
+    "revision": "9e9b2387ed94720adbeb27d50b6b319c",
+    "url": "/portfolio/static/media/bg.9e9b2387.jpg"
+  },
+  {
+    "revision": "c0cbc9e4ec93fb4ab309456d57df2d70",
+    "url": "/portfolio/static/media/internet.c0cbc9e4.jpg"
+  },
+  {
+    "revision": "66bd4eeac953040b20614dd4cfa63803",
+    "url": "/portfolio/static/media/circuit.66bd4eea.jpg"
+  },
+  {
+    "revision": "13415514430a1af77a3cab7e204c7ef5",
+    "url": "/portfolio/static/media/database.13415514.jpg"
+  },
+  {
+    "revision": "dbb19eaec48760be0d4482794b2c8c2a",
+    "url": "/portfolio/static/media/deployment.dbb19eae.jpg"
+  },
+  {
+    "revision": "6da359dfaf2259300919985cfde86e0e",
+    "url": "/portfolio/static/media/server.6da359df.jpg"
+  },
+  {
+    "revision": "0ad46e9e18ce6e9d6dd306e7a53a865a",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.927adfda50e9f5b542cf25ff97652754.js"
+  "/portfolio/precache-manifest.7078459336e44cfde0647219f157d622.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
