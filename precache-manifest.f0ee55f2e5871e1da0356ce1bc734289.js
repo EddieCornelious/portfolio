86362b481e591db7ed55e4345e9c4c876b1e0862
EddieCornelious <eulerissmart@gmail.com>
2020-01-11 15:44:36 -0500

    Updates

---
+++ precache-manifest.f0ee55f2e5871e1da0356ce1bc734289.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "17dc5bd706d8242ffc5a",
+    "url": "/static/js/0.24c282a4.chunk.js"
+  },
+  {
+    "revision": "cd19352f0c44fccf9a7c",
+    "url": "/static/css/main.8faea813.chunk.css"
+  },
+  {
+    "revision": "cd19352f0c44fccf9a7c",
+    "url": "/static/js/main.6f3db9d2.chunk.js"
+  },
+  {
+    "revision": "374df1d4ca981f11c147",
+    "url": "/static/js/runtime~main.d8dafaa9.js"
+  },
+  {
+    "revision": "0b06047bc94a8d60f0f6",
+    "url": "/static/css/3.75964088.chunk.css"
+  },
+  {
+    "revision": "0b06047bc94a8d60f0f6",
+    "url": "/static/js/3.b5b2d51f.chunk.js"
+  },
+  {
+    "revision": "4fe8d3f2c7c36b51be57",
+    "url": "/static/js/4.c07cc29a.chunk.js"
+  },
+  {
+    "revision": "b57ac414a1d21b5e988f",
+    "url": "/static/js/5.dd091cda.chunk.js"
+  },
+  {
+    "revision": "168075d8133521dfaaf8",
+    "url": "/static/css/6.5634189d.chunk.css"
+  },
+  {
+    "revision": "168075d8133521dfaaf8",
+    "url": "/static/js/6.738e13a6.chunk.js"
+  },
+  {
+    "revision": "eeffda1f7e081791a08d",
+    "url": "/static/css/7.74f7c015.chunk.css"
+  },
+  {
+    "revision": "eeffda1f7e081791a08d",
+    "url": "/static/js/7.1816412f.chunk.js"
+  },
+  {
+    "revision": "8194b7293382561e0a66",
+    "url": "/static/css/8.9b934ba6.chunk.css"
+  },
+  {
+    "revision": "8194b7293382561e0a66",
+    "url": "/static/js/8.53500f17.chunk.js"
+  },
+  {
+    "revision": "54d7c9256c35108fccd1",
+    "url": "/static/css/9.5b37c96f.chunk.css"
+  },
+  {
+    "revision": "54d7c9256c35108fccd1",
+    "url": "/static/js/9.78fa7eeb.chunk.js"
+  },
+  {
+    "revision": "334d9b3a8740e688da7c",
+    "url": "/static/css/10.d5d6c8e5.chunk.css"
+  },
+  {
+    "revision": "334d9b3a8740e688da7c",
+    "url": "/static/js/10.5ac1b4a4.chunk.js"
+  },
+  {
+    "revision": "ed432c9049a133d950d1cf831c1071cf",
+    "url": "/static/media/manuels.ed432c90.PNG"
+  },
+  {
+    "revision": "386b5f9fb6020d2436b04a9e7049e943",
+    "url": "/static/media/realtor.386b5f9f.PNG"
+  },
+  {
+    "revision": "e08083f65257c03dd3d1109483f3b43b",
+    "url": "/static/media/stream.e08083f6.PNG"
+  },
+  {
+    "revision": "1a43c40f889e4479c56d3b1a44914905",
+    "url": "/static/media/gym.1a43c40f.PNG"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "9e9b2387ed94720adbeb27d50b6b319c",
+    "url": "/static/media/bg.9e9b2387.jpg"
+  },
+  {
+    "revision": "a9af1c0c62c56c7e18a794bec1708496",
+    "url": "/static/media/react.a9af1c0c.png"
+  },
+  {
+    "revision": "8087eb0efce0b13846121d02d32ca739",
+    "url": "/static/media/circuit.8087eb0e.jpg"
+  },
+  {
+    "revision": "34376dcdaeae288bfef9ee5f66f24884",
+    "url": "/static/media/internet.34376dcd.jpg"
+  },
+  {
+    "revision": "1513d71c0f6a1008e440690985979f64",
+    "url": "/static/media/database.1513d71c.jpg"
+  },
+  {
+    "revision": "c49d1d663d05e6c9817829bab7fa01f8",
+    "url": "/static/media/deployment.c49d1d66.jpg"
+  },
+  {
+    "revision": "8aed32bad785feb71764a87a10316ccf",
+    "url": "/static/media/server.8aed32ba.jpg"
+  },
+  {
+    "revision": "fb4b06c7b1ff5cf04bd64423dfe8fee9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bcfa56799c9f827deba63531d27b54f8.js"
+  "/precache-manifest.f0ee55f2e5871e1da0356ce1bc734289.js"
 );
 
 workbox.clientsClaim();
