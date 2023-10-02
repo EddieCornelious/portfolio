350f503ff7c376e75f58c15b0be2f5533096d8fd
Eddie <eulerissmart@gmail.com>
2023-10-02 01:21:09 -0400

    Update project_data.js

--- src/components/project_data.js
+++ src/components/project_data.js
@@ -10,7 +10,7 @@ const data = {
       "A restaurant site I built for a client using HTML, CSS, and React. I also used NPM and Node JS on the backend",
     img_src: manuels,
     github: "https://github.com/EddieCornelious/restaurant",
-    url: "https://www.manuelsgrill.com"
+    url: "https://manuels-restaurant.netlify.app"
   },
   1: {
     title: "John & Wrink",
@@ -18,7 +18,7 @@ const data = {
       "A real-estate site I built for a client using HTML, Node JS, CSS, and React. I really wanted to focus on the design for this site",
     img_src: wick,
     github: "https://github.com/EddieCornelious/homes",
-    url: "https://www.johnandwrink.com"
+    url: "https://johnandwrink.netlify.app"
   },
   2: {
     title: "DashBoard Box",
@@ -26,7 +26,7 @@ const data = {
       "A dashboard/portal for that allows users to view popular Twitch games and streams live and sort or filter results based on certain criteria",
     img_src: stream,
     github: "https://github.com/EddieCornelious/stream",
-    url: "https://eddiecornelious.github.io/stream/"
+    url: "https://twitchy-witchy.netlify.app"
   },
   4: {
     title: "Xenos Fitness",
@@ -34,7 +34,7 @@ const data = {
       "A site I built for a fitness center that uses HTML, CSS, and React. I also used NPM and Node JS on the backend",
     img_src: fitness,
     github: "https://github.com/EddieCornelious/fitness",
-    url: "https://www.xenosfitness.com"
+    url: "https://xenosfitness.netlify.app"
   }
 };
 
